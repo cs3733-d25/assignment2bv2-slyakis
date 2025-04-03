5b976080ba9b93773eb665d069250ce680884b0c
Kaylie Quach <97192455+slyakis@users.noreply.github.com>
2025-04-03 00:47:10 -0400

    page structure

---
+++ src/christine_components/Hobby2.tsx
@@ -0,0 +1,22 @@
+const Hobby2 = () => {
+    return (
+        <>
+            <div className="hobby-container">
+                <div className="hobby-text">
+                    <p>One of my hobbies is baking! I like doing it with friends as it is a fun
+                    and rewarding activity to bond over.
+                    My favorite experience was a cupcake decorating workshop I attended where I got to learn about different
+                    decorating methods and try them out myself!
+                    I also did the blind, death, mute baking challenge while making a cake with my friends, where two of us
+                    were blindfolded, one couldn't speak, and one couldn't hear.</p>
+                </div>
+                <div className="image-container">
+                    <img src="/baking.jpg"/>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Hobby2;
