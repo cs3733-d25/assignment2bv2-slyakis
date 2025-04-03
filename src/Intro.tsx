2b6a57ee3555e9155c38b048efbc59e347679188
Kaylie Quach <97192455+slyakis@users.noreply.github.com>
2025-04-02 23:01:48 -0400

    intro page update

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -3,19 +3,20 @@ import "./App.css";
 
 function Intro(props) {
     const [fullName, setFullName] = useState(props.username);
+    const names = fullName.split(" / ");
 
     return (
         <>
             <h2>About Us</h2>
             <p>
-                My name is Kaylie Quach. I am a second-year Computer Science student at
+                My name is {names[0]}. I am a second-year Computer Science student at
                 Worcester Polytechnic Institute in Worcester, Massachusetts. I am an international
                 student from Vietnam, though I've been living in the United States for the past
                 six years since the start of high school.
             </p>
             <p>
-                My name is Christine Ngo. I'm a sophomore majoring in Computer Science at WPI.
-                I'm from Manchester, NH.
+                My name is {names[1]}. I'm a sophomore majoring in Computer Science at WPI.
+                I'm from Manchester, NH. I'm
             </p>
         </>
     );
