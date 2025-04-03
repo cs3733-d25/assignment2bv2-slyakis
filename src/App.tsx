633f296f2524d9f0068c523ce8685ffdcc4d4b96
Kaylie Quach <97192455+slyakis@users.noreply.github.com>
2025-04-02 23:37:24 -0400

    k's hobby and list

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import baking from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Title from "./Title.tsx"
 import React from "react";
+import Title from "./Title.tsx";
 import Intro from "./Intro";
+import Hobby1 from "./Hobby1.tsx";
+import List1 from "./List1";
 
 function App() {
   return (
